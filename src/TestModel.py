d4a8bc5ab6f2b02ad56d973f59f87cf0a683ae29
sagi <sagi21805@gmail.com>
2023-09-29 16:36:52 +0300

    c

--- src/TestModel.py
+++ src/TestModel.py
@@ -1,85 +1,174 @@
-import pandas as pd
-import os 
-import numpy as np
-from src.config import TESTING_IMAGES_PATH, TRAIN_IMG_PATH
-import tensorflow as tf
-from src.data_handle import decode_dicom, decode_jpeg
+import os
+import gc
 import cv2
+import pydicom
+from joblib import Parallel, delayed
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from tqdm.notebook import tqdm
+from glob import glob
+from config import RESIZE_DIM, IMAGE_SIZE, AUTOTUNE, BATCH_SIZE
 
-TESTING_IMAGES_PATH = TRAIN_IMG_PATH
+BASE_PATH = "/kaggle/input/rsna-2023-abdominal-trauma-detection"
+IMAGE_DIR = "/tmp/dataset/rsna-atd"
+MODEL_PATH = "/kaggle/working/rsna-atd.keras"
+STRIDE = 10
+TARGET_CLASSES  = ["bowel_healthy", "bowel_injury", 
+                   "extravasation_healthy","extravasation_injury", 
+                   "kidney_healthy", "kidney_low","kidney_high", 
+                   "liver_healthy", "liver_low", "liver_high",
+                   "spleen_healthy", "spleen_low", "spleen_high"]
 
-np.set_printoptions(suppress=True)
-
-model = tf.keras.models.load_model("/home/sagi21805/Desktop/VsCode/competitions/RSNA-2023-Abdominal-Trauma-Detection/test_model.keras")
-
+model = tf.keras.models.load_model(MODEL_PATH)
 model.summary()
 
-samp_sub_df = pd.read_csv("sample_submission.csv")
-header = samp_sub_df.columns.values.tolist()
-empty = {        
-            "patient_id" : [0],
-            "bowel_healthy": [0],
-            "bowel_injury" : [0], 
-            "extravasation_healthy" : [0], 
-            "extravasation_injury" : [0], 
-            "kidney_healthy" : [0],
-            "kidney_low" : [0],
-            "kidדney_high" : [0],
-            "liver_healthy" : [0] ,
-            "liver_low" : [0],
-            "liver_high" : [0],
-            "spleen_healthy"  : [0],
-            "spleen_low" : [0],
-            "spleen_high" : [0]  
-            }
-sub_df = pd.DataFrame(columns=header)
-for patient_id in os.listdir(TESTING_IMAGES_PATH):
-    data = pd.DataFrame(empty)
-    reps = 0
-    # for series_id in os.listdir(TESTING_IMAGES_PATH + "/" + patient_id):
-    for img_path in os.listdir(TESTING_IMAGES_PATH + "/" + patient_id):
-        image = decode_jpeg(TESTING_IMAGES_PATH + "/" + patient_id + "/" + img_path)
-        bowel_injury, extra_injury, kidney, liver, spleen = model.predict(image, verbose='silent')
-        
-        arr = np.array([1-bowel_injury[0][0], bowel_injury[0][0]])
-        bowel_healty, bowel_injury = np.vectorize(lambda x: int(x >= 1/len(arr)))(arr)
-        
-        arr = np.array([1-extra_injury[0][0], extra_injury[0][0]])
-        extra_healty, extra_injury = np.vectorize(lambda x: int(x >= 1/len(arr)))(arr)
+meta_df = pd.read_csv(f"{BASE_PATH}/test_series_meta.csv")
 
-        arr = kidney[0]
-        kidney_healthy, kidney_low, kidney_high = np.vectorize(lambda x: int(x >= 1/len(arr)))(arr)
+num_rows = meta_df.shape[0]
+unique_patients = meta_df["patient_id"].nunique()
 
-        arr = liver[0]
-        liver_healthy, liver_low, liver_high = np.vectorize(lambda x: int(x >= 1/len(arr)))(arr)
+meta_df["dicom_folder"] = BASE_PATH + "/" + "test_images"\
+                                    + "/" + meta_df.patient_id.astype(str)\
+                                    + "/" + meta_df.series_id.astype(str)
 
-        arr = spleen[0]
-        spleen_healthy, spleen_low, spleen_high = np.vectorize(lambda x: int(x >= 1/len(arr)))(arr)
+test_folders = meta_df.dicom_folder.tolist()
+test_paths = []
+for folder in tqdm(test_folders):
+    test_paths += sorted(glob(os.path.join(folder, "*dcm")))[::STRIDE]
+test_df = pd.DataFrame(test_paths, columns=["dicom_path"])
+test_df["patient_id"] = test_df.dicom_path.map(lambda x: x.split("/")[-3]).astype(int)
+test_df["series_id"] = test_df.dicom_path.map(lambda x: x.split("/")[-2]).astype(int)
+test_df["instance_number"] = test_df.dicom_path.map(lambda x: x.split("/")[-1].replace(".dcm","")).astype(int)
 
-        data_d = {        
-        "patient_id" : int(patient_id),
-        "bowel_healthy": [bowel_healty],
-        "bowel_injury" : [bowel_injury], 
-        "extravasation_healthy" : [extra_healty], 
-        "extravasation_injury" : [extra_injury], 
-        "kidney_healthy" : [kidney_healthy],
-        "kidney_low" : [kidney_low],
-        "kidney_high" : [kidney_high],
-        "liver_healthy" :  [liver_healthy],
-        "liver_low" : [liver_low],
-        "liver_high" : [liver_high],
-        "spleen_healthy"  : [spleen_healthy],
-        "spleen_low" : [spleen_low],
-        "spleen_high" : [spleen_high]
-        }
-        data += pd.DataFrame(data_d)
-        reps += 1
-        break
-    data = data / reps
-    data = data.astype({'patient_id':'int'})
-    sub_df = pd.concat([sub_df, data])
+test_df["image_path"] = f"{IMAGE_DIR}/test_images"\
+                    + "/" + test_df.patient_id.astype(str)\
+                    + "/" + test_df.series_id.astype(str)\
+                    + "/" + test_df.instance_number.astype(str) +".png"
+                    
+def standardize_pixel_array(dcm):
+    # Correct DICOM pixel_array if PixelRepresentation == 1.
+    pixel_array = dcm.pixel_array
+    if dcm.PixelRepresentation == 1:
+        bit_shift = dcm.BitsAllocated - dcm.BitsStored
+        dtype = pixel_array.dtype 
+        new_array = (pixel_array << bit_shift).astype(dtype) >>  bit_shift
+        pixel_array = pydicom.pixel_data_handlers.util.apply_modality_lut(new_array, dcm)
+    return pixel_array
 
-sub_df = sub_df.sort_values(by=['patient_id'])
-sub_df.to_csv("submission.csv", index = False)
+def read_xray(path, fix_monochrome=True):
+    dicom = pydicom.dcmread(path)
+    data = standardize_pixel_array(dicom)
+    data = data - np.min(data)
+    data = data / (np.max(data) + 1e-5)
+    if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = 1.0 - data
+    return data
 
+def resize_and_save(file_path):
+    img = read_xray(file_path)
+    h, w = img.shape[:2]  # orig hw
+    img = cv2.resize(img, (RESIZE_DIM, RESIZE_DIM), cv2.INTER_LINEAR)
+    img = (img * 255).astype(np.uint8)
+    
+    sub_path = file_path.split("/",4)[-1].split(".dcm")[0] + ".png"
+    infos = sub_path.split("/")
+    sub_path = file_path.split("/",4)[-1].split(".dcm")[0] + ".png"
+    infos = sub_path.split("/")
+    pid = infos[-3]
+    sid = infos[-2]
+    iid = infos[-1]; iid = iid.replace(".png","")
+    new_path = os.path.join(IMAGE_DIR, sub_path)
+    os.makedirs(new_path.rsplit("/",1)[0], exist_ok=True)
+    cv2.imwrite(new_path, img)
+    return
+
+file_paths = test_df.dicom_path.tolist()
+_ = Parallel(n_jobs=2, backend="threading")(
+    delayed(resize_and_save)(file_path) for file_path in tqdm(file_paths, leave=True, position=0)
+)
+
+del _; gc.collect()
+
+def decode_image(image_path):
+    file_bytes = tf.io.read_file(image_path)
+    image = tf.io.decode_png(file_bytes, channels=3, dtype=tf.uint8)
+    image = tf.image.resize(image, IMAGE_SIZE, method="bilinear")
+    image = tf.cast(image, tf.float32) / 255.0
+    return image
+
+def build_dataset(image_paths):
+    ds = (
+        tf.data.Dataset.from_tensor_slices(image_paths)
+        .map(decode_image, num_parallel_calls=AUTOTUNE)
+        .shuffle(BATCH_SIZE * 10)
+        .batch(BATCH_SIZE)
+        .prefetch(AUTOTUNE)
+    )
+    return ds
+
+paths  = test_df.image_path.tolist()
+
+ds = build_dataset(paths)
+
+def post_proc(pred):
+    proc_pred = np.empty((pred.shape[0], 2*2 + 3*3), dtype= np.float32)
+
+    # bowel, extravasation
+    proc_pred[:, 0] = pred[:, 0]
+    proc_pred[:, 1] = 1 - proc_pred[:, 0]
+    proc_pred[:, 2] = pred[:, 1]
+    proc_pred[:, 3] = 1 - proc_pred[:, 2]
+    
+    # liver, kidney, sneel
+    proc_pred[:, 4:7] = pred[:, 2:5]
+    proc_pred[:, 7:10] = pred[:, 5:8]
+    proc_pred[:, 10:13] = pred[:, 8:11]
+
+    return proc_pred
+
+patient_ids = test_df["patient_id"].unique()
+
+# Initializing array to store predictions
+patient_preds = np.zeros(
+    shape=(len(patient_ids), 2*2 + 3*3),
+    dtype="float32"
+)
+
+# Iterating over each patient
+for pidx, patient_id in tqdm(enumerate(patient_ids), total=len(patient_ids), desc="Patients "):
+    print(f"Patient ID: {patient_id}")
+    
+    # Query the dataframe for a particular patient
+    patient_df = test_df.query("patient_id == @patient_id")
+    
+    # Getting image paths for a patient
+    patient_paths = patient_df.image_path.tolist()
+
+    # Building dataset for prediction
+    dtest = build_dataset(patient_paths)
+    
+    # Predicting with the model
+    pred = model.predict(dtest)
+    pred = np.concatenate(pred, axis=-1).astype("float32")
+    pred = pred[:len(patient_paths), :]
+    pred = np.mean(pred.reshape(1, len(patient_paths), 11), axis=0)
+    pred = np.max(pred, axis=0, keepdims=True)
+    
+    patient_preds[pidx, :] += post_proc(pred)[0]
+    
+
+    # Deleting variables to free up memory 
+    del patient_df, patient_paths, dtest, pred; gc.collect()
+    
+pred_df = pd.DataFrame({"patient_id":patient_ids,})
+pred_df[TARGET_CLASSES] = patient_preds.astype("float32")
+
+# Align with sample submission
+sub_df = pd.read_csv(f"{BASE_PATH}/sample_submission.csv")
+sub_df = sub_df[["patient_id"]]
+sub_df = sub_df.merge(pred_df, on="patient_id", how="left")
+
+# Store submission
+sub_df.to_csv("submission.csv",index=False)
 
